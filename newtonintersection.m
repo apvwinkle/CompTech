1c31a4c3a29556e7c94bfda6220e92769875125f
apvwinkle <apvwinkle0313@gmail.com>
2014-09-16 18:58:06 -0700

    (rev) Newton Intersection
    it didn't work so I'm doing something different again. I made a better
    commit on the .asv file two down if you want to read it.

--- newtonintersection.m
+++ newtonintersection.m
@@ -1,20 +1,25 @@
-function intersection = newtonintersection(polyf, polyg, guess, iterations)
-%This function solves for the intersection of two polynomial functions (entered as arrays) with initial
-%guesses at an x value and y value that is close to the intersection.
-xf = guess; xg = guess;
-for i=1:iterations
-    f = polyval(polyf, xf)
-    g = polyval(polyg, xg)
-    df = polyder(polyf, xf)
-    dg = polyder(polyg, xg)
-    xf = xf - f/df
-    xg = xg - g/dg
+function intersection = newtonintersection(guesses, variables, iterations, varagin)
+%This function solves for the intersection of nonlinear functions (entered as symbolic into varagin) with initial
+%guesses a point that is close to the intersection.
+for i = 1:(nargin()-3)
+fun(i,1) = varagin{i} %each row will be a function
 end
-% xf
-% xg
-% yf
-% yg
-xavg = (xf + xg)/2;
-yavg = (polyval(f, xf) + polyval(g, xg))/2;
-intersection(1) = xavg; intersection(2) = yavg;
-end
+%J = jacobian(fun, variables)
+
+% xf = guess; xg = guess;
+% for i=1:iterations
+%     f = polyval(polyf, xf)
+%     g = polyval(polyg, xg)
+%     df = polyder(polyf, xf)
+%     dg = polyder(polyg, xg)
+%     xf = xf - f/df
+%     xg = xg - g/dg
+% end
+% % xf
+% % xg
+% % yf
+% % yg
+% xavg = (xf + xg)/2;
+% yavg = (polyval(f, xf) + polyval(g, xg))/2;
+% intersection(1) = xavg; intersection(2) = yavg;
+% end
