374d2a0bc7de41c2c150535e329e8adfced5aca7
apvwinkle <apvwinkle0313@gmail.com>
2014-09-21 22:38:09 -0700

    (rev) fixed point nonlinear script
    it works now! YAY!
    For fixed point nonlinear, you must solve for x, not just have an x on
    one side

--- fixedpointnonlinearscript.m
+++ fixedpointnonlinearscript.m
@@ -1,14 +1,15 @@
 xguess = 1; yguess = 1;
 iterations = 5;
-f1 = @(x,y) x^2 + 2*x + 2*y^2 -26; %solve it for a singular x on one side to get g
-g1 = @(x,y) 13 - x^2/2 - y^2; % = x
-f2 = @(x,y) 2*x^3 - y^2 + 4*y -19;
-g2 = @(x,y) (19 + y^2 - 2*x^3)/4; % = y
-
+%f1 = 2*x^3 - y^2 + 4*y -19;
+%f2 = x^2 + 2*x + 2*y^2 -26;
+%Solve f1 and f2 for x and y, respectively, to acquire our g functions
+syms x y
+g1 = (9.5 - .5*y^2 -2*y)^1/3; % = x
+g2 = sqrt(13 - .5*x^2 - x); % = y
 X = xguess; Y = yguess;
 for i = 1:iterations
-   X = subs(g1, [x y], [X Y]);
-   Y = subs(g2, [x y], [X Y]);
+   X = subs(g1, y, Y);
+   Y = subs(g2, x, X);
 end
 X = double(X)
 Y = double(Y)
