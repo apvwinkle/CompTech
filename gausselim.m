e43c1e640799069b49d251c7bb90af2126dae959
apvwinkle <apvwinkle0313@gmail.com>
2014-09-27 16:41:04 -0700

    (rev) Gauss Elimination
    took out some unnecessary stuff

--- gausselim.m
+++ gausselim.m
@@ -3,25 +3,19 @@ function x = gausselim(A,b)
 %variables = b to solve for the variables
 Aaug = [A b];
 n = size(A,1); %gets the number of loops we'll need from the size of the matrix
-RowOrder = 1:n;
-Ap = [Aaug RowOrder'];
 for c = 1:n %we want to change all columns except the first and the Row Order column
     %PIVOTING
-    part = Ap(c:n, :); %row c to the last row get pivoted
-    [d i] = sortrows(abs(part), c); %it is sorting from the c column because we will be using  diagonal
-    %we use i to make our real matrix go in the right order
-    part = part(flipud(i), :);
-    Ap(c:n,:) = part; %replace row n to the end with whatever is in part
+   Aaug = pivot(Aaug,c);
     %MAKING THE UPPER TRIANGLE
-    pivotelem =  Ap(c,c); %our pivot element changes each row and goes diagonally down the matrix
+    pivotelem =  Aaug(c,c); %our pivot element changes each row and goes diagonally down the matrix
     for r = (c+1):n %all the rows underneath the pivot element get changed
-        if Ap(r,c) ~= 0
-        factor = pivotelem/Ap(r,c);
-        Ap(r,c:n+1) =  Ap(c,c:n+1) - Ap(r,c:n+1)*factor;
+        if Aaug(r,c) ~= 0
+        factor = pivotelem/Aaug(r,c);
+        Aaug(r,:) =  Aaug(c,:) - Aaug(r,:)*factor;
         end
-        % %all the numbers in the pivot row except the Row Order get changed
+        %all the numbers in the pivot row except the Row Order get changed
     end
 end
 %BACK SUBSTITUTION to get the answers
-x = backsubtest(Ap);
+x = backsubtest(Aaug);
 
