ced8c54135f2466d041231dd52df5ab0f90acc2c
apvwinkle <apvwinkle0313@gmail.com>
2014-09-27 16:41:34 -0700

    (rev) pivot function
    took out some unnecessary stuff

--- pivot.m
+++ pivot.m
@@ -1,14 +1,13 @@
-function ret = pivot(A, n)
+function ret = pivot(A, c)
 %This pivots the rows, useful for eliminating round off error. A is the
-%matrix to be pivoted, and n is the row and column you're working on (it
-%pivots on the diagonal). It will return with your pivoted matrix and the
-%row order as the last column
-part = A(n:end, :); %row n to the last row get pivoted
-[d i] = sortrows(abs(part), n) %it is sorting from the n column because we will be using  diagonal
-%we use i to make our real matrix go in the right order
-part = part(flipud(i), :);
-Ap = A; %makes a copy of our input array
-Ap(n:end,:) = part; %replace row n to the end with whatever is in part
-RowOrder = [1:(n-1) (flipud(i)+n-1)']';
-ret = [Ap RowOrder];
+%matrix to be pivoted, and col is the row and column you're working on (it
+%pivots on the diagonal). It will return with your pivoted matrix.
+n = size(A,1); %gets the number of loops we'll need from the size of the matrix
+Ap = A;
+    part = Ap(c:n, :); %row c to the last row get pivoted
+    [d i] = sortrows(abs(part), c); %it is sorting from the c column because we will be using  diagonal
+    %we use i to make our real matrix go in the right order
+    part = part(flipud(i), :);
+    Ap(c:n,:) = part; %replace row n to the end with whatever is in part
+ret = Ap;
 end
