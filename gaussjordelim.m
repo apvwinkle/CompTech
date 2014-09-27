df230390887f51174be56d443b6dc36122b38012
apvwinkle <apvwinkle0313@gmail.com>
2014-09-26 23:00:53 -0700

    (new) Gauss Jordan Elimination
    might not be finished as we wrote this in class

--- gaussjordelim.m
+++ gaussjordelim.m
@@ -1,3 +1,6 @@
 function ret = gaussjordelim(A,b)
 %This performs Gauss -Jordan elimination for a square matrix A *
 %variables = b to solve for the variables
+M = [A b'];
+[d i] = (sortrows(abs(M), 2))
+flipud([d i])
