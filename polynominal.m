ca87d62f057949bf6bc3de213dc0a3fbdfe113fe
apvwinkle <apvwinkle0313@gmail.com>
2014-11-02 23:57:09 -0800

    (new) polynominal regression
    book way

---
+++ polynominal.m
@@ -0,0 +1,21 @@
+function coeffs = polynominal(x,y,m)
+%This fits a polynominal function of degree m to a set of (x,y) data. The
+%coefficients are returned in the order a*x^m + b*x^(m-1) + ... + z
+for i = 1:2*m %we need 2*m for our system of equations
+    xsum(i) = sum(x.^(i));
+end
+n = length(x);
+a(1,1) = n;
+b(1,1) = sum(y);
+for j = 2:m+1
+    a(1,j) = xsum(j-1); %for the first row of matrix a
+    for k = 1:m+1
+        a(j,k) = xsum(k + j - 2); %for all the other rows
+    end
+    b(j,1) = sum(x.^(j - 1) .*y); %answer column
+end
+p = (a\b)'; %solves the system of eqs
+for i = 1:m+1
+coeffs(i) = p(m + 2 - i);
+end
+end
