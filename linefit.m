9815cc21c7fddf508a2aac6119faabe3f477fde8
apvwinkle <apvwinkle0313@gmail.com>
2014-12-09 22:02:19 -0800

    (rev) linefit
    added error

--- linefit.m
+++ linefit.m
@@ -1,6 +1,7 @@
 function coeffs = linefit(x,y)
 %This finds the slope and intercept of (x,y) data and minimizes the squared
-%error. coeffs = [m b]
+%error. coeffs = [m b]. This gives the same results as linear.m, but
+%without the graphing
 s = size(x);
 if s(1) > s(2)
     n = s(1);
@@ -8,7 +9,11 @@ if s(1) > s(2)
     coeffs = ( (A'*A)^-1 * A' * y )';
 elseif s(2) > s(1)
     n = s(2);
-    A = [x; ones(1,n)]
+    A = [x; ones(1,n)];
     coeffs = y * (A' * (A*A')^-1);
 end
+for i = 1:length(x)
+E(i) = ( y(i) - (coeffs(1)*x(i) + coeffs(2)) )^2;
+end
+Error = sum(E)
 end
